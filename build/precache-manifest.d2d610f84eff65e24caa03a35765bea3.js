8d01e7b767b61117ff5d1bbcca2f2a35df20f453
pratik210594 <pratik.waghela94@gmail.com>
2022-05-29 16:11:05 +0530

    first commit

---
+++ build/precache-manifest.d2d610f84eff65e24caa03a35765bea3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5429bb05c84b7b2f3391a5452f4f7fc7",
+    "url": "/pratik210594/prabal/index.html"
+  },
+  {
+    "revision": "cffa73d4bef52acee42b",
+    "url": "/pratik210594/prabal/static/css/main.e8ac9815.chunk.css"
+  },
+  {
+    "revision": "30bbc9ca41be2028c686",
+    "url": "/pratik210594/prabal/static/js/2.ddb36e4b.chunk.js"
+  },
+  {
+    "revision": "cffa73d4bef52acee42b",
+    "url": "/pratik210594/prabal/static/js/main.6db05fea.chunk.js"
+  },
+  {
+    "revision": "497aaaefbdbae029da2c",
+    "url": "/pratik210594/prabal/static/js/runtime~main.7f64b5dd.js"
+  }
+]);
